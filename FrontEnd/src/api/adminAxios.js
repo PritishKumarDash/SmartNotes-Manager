85c56c750eca6033d877c558c7312d4e0a094420
Pritish Kumar Dash <pritishdash27@gmail.com>
2026-05-10 20:05:31 +0530

    Update adminAxios.js

--- FrontEnd/src/api/adminAxios.js
+++ FrontEnd/src/api/adminAxios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const ADMIN_API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:3000",
+  baseURL: import.meta.env.VITE_API_URL || "https://smartnotes-manager.onrender.com",
   withCredentials: true,
 });
 
